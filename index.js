ecfdc41fadbf996934f8ffbf481517ca4accf6e1
DANIEL JOHNSTON <danjohnston93@cox.net>
2018-03-01 07:10:27 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+
+function theBeatlesPlay(array1, array2) {
+  var output = [];
+  for (i=0; i<array1.length; i++){
+      output.push(array1[i] + " plays " + array2[i]);
+    } return output; 
+  } 
+  
+  function johnLennonFacts(facts) {
+    i=0;
+    var output = [];
+    while (i<facts.length){
+      output = facts[i] + "!!!"; 
+      i++;
+    } return output;
+  }
+  
+  function iLoveTheBeatles(number){
+    do {
+      var output = [];
+      output += "I love the Beatles!";
+      number++;
+    } while (number<15);
+    return output;
+  }
